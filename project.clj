d9c18289f3966b85de57fd8a6e0d5037ffdbf7d0
Mick Thomure <mthomure@gmail.com>
2016-12-22 18:07:24 -0800

    bumped version

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject mthomure/clj-gym "0.1.0-SNAPSHOT"
+(defproject mthomure/clj-gym "0.1.0"
   :description "Clojure Bindings for OpenAI's gym-http-api server"
   :url "http://github.com/mthomure/clj-gym"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-http "2.3.0"]
                  [cheshire "5.6.3"]])
